2b544c0648f3eedf08d303e59cc98238650fb3b4
qnp <francois.risoud@gmail.com>
2018-04-25 10:38:40 +0200

    updated tree and examples

--- webpack.prod.js
+++ webpack.prod.js
@@ -66,62 +66,34 @@ module.exports = merge(common.config, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     })),
+
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
+
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   minChunks (module) {
-    //     // any required modules inside node_modules are extracted to vendor
-    //     return (
-    //       module.resource &&
-    //       /\.js$/.test(module.resource) &&
-    //       module.resource.indexOf(
-    //         path.join(__dirname, '../node_modules')
-    //       ) === 0
-    //     )
-    //   }
-    // }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest',
-    //   minChunks: Infinity
-    // }),
-    // This instance extracts shared chunks from code splitted chunks and bundles them
-    // in a separate chunk, similar to the vendor chunk
-    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'app',
-    //   async: 'vendor-async',
-    //   children: true,
-    //   minChunks: 3
-    // }),
 
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, './src/static'),
+        from: path.resolve(__dirname, './static'),
         to: 'static',
         ignore: ['.*']
       },
-      // {
-      //   from: path.resolve(__dirname, './src/favicons'),
-      //   to: distRoot,
-      // },
-      // {
-      //   from: path.resolve(__dirname, './src/sitemap.xml'),
-      //   to: distRoot,
-      // },
-      // {
-      //   from: path.resolve(__dirname, './src/_redirects'),
-      //   to: distRoot,
-      // },
+      {
+        from: path.resolve(__dirname, './src/favicons'),
+        to: distRoot,
+        ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, './src/root'),
+        to: distRoot,
+        ignore: ['.*']
+      },
     ]),
 
     new BundleAnalyzerPlugin({
+      reportFilename: '../report/report.html',
       analyzerMode: 'static',
       openAnalyzer: false
     }),
